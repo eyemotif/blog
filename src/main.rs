f5f75666e510de4c9091409cd5b2d8346d9e2c96
eyemotif <yepicness@gmail.com>
2023-11-06 16:00:45 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use axum::ServiceExt;
+use tower::Layer;
+use tower_http::normalize_path::NormalizePathLayer;
+
+#[tokio::main]
+async fn main() {
+    let app = NormalizePathLayer::trim_trailing_slash().layer(axum::Router::new());
+
+    axum::Server::bind(&std::net::SocketAddr::from(([0, 0, 0, 0], 8010)))
+        .serve(app.into_make_service())
+        .await
+        .expect("Error serving app")
+}
