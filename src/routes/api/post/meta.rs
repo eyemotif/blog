593c7bc1219b33f362b56b1ea0e75f952a391d5a
eyemotif <yepicness@gmail.com>
2023-11-06 17:44:37 -0500

    Add /post/meta and /post/text endpoints

--- src/routes/api/post/meta.rs
+++ src/routes/api/post/meta.rs
@@ -1,8 +1,34 @@
-use crate::blog::PostID;
+use crate::blog::{PostID, STORE_PATH};
 use axum::extract::Path;
 use axum::http::StatusCode;
 use axum::Json;
 
 pub async fn get(Path(post_id): Path<PostID>) -> Result<Json<crate::blog::Post>, StatusCode> {
-    todo!("api/post/meta");
+    let file = match tokio::fs::read(
+        std::path::Path::new(STORE_PATH)
+            .join(&post_id)
+            .join("meta.json"),
+    )
+    .await
+    {
+        Ok(it) => it,
+        Err(err) => {
+            if err.kind() == std::io::ErrorKind::NotFound {
+                return Err(StatusCode::NOT_FOUND);
+            } else {
+                eprintln!("Error reading post {post_id} meta: {err}");
+                return Err(StatusCode::INTERNAL_SERVER_ERROR);
+            }
+        }
+    };
+
+    let post = match serde_json::from_slice(&file) {
+        Ok(it) => it,
+        Err(err) => {
+            eprintln!("Error deserializing post {post_id} meta: {err}");
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
+
+    Ok(Json(post))
 }
--- src/routes/api/post/text.rs
+++ src/routes/api/post/text.rs
@@ -1,8 +1,25 @@
-use crate::blog::PostID;
+use crate::blog::{PostID, STORE_PATH};
 use axum::extract::Path;
 use axum::http::StatusCode;
-use axum::response::Response;
 
-pub async fn get(Path(post_id): Path<PostID>) -> Result<Response, StatusCode> {
-    todo!("api/post/text");
+pub async fn get(Path(post_id): Path<PostID>) -> Result<String, StatusCode> {
+    let file = match tokio::fs::read_to_string(
+        std::path::Path::new(STORE_PATH)
+            .join(&post_id)
+            .join("post.md"),
+    )
+    .await
+    {
+        Ok(it) => it,
+        Err(err) => {
+            if err.kind() == std::io::ErrorKind::NotFound {
+                return Err(StatusCode::NOT_FOUND);
+            } else {
+                eprintln!("Error reading post {post_id} meta: {err}");
+                return Err(StatusCode::INTERNAL_SERVER_ERROR);
+            }
+        }
+    };
+
+    Ok(file)
 }
