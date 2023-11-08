8e6fa24296a2ae9a16d292c34a50141ea3409a3f
eyemotif <yepicness@gmail.com>
2023-11-08 16:26:24 -0500

    Update post/text endpoint to return html

--- src/routes/api/post/text.rs
+++ src/routes/api/post/text.rs
@@ -1,8 +1,9 @@
 use crate::blog::{PostID, STORE_PATH};
 use axum::extract::Path;
 use axum::http::StatusCode;
+use axum::response::{Html, IntoResponse, Response};
 
-pub(super) async fn get(Path(post_id): Path<PostID>) -> Result<String, StatusCode> {
+pub(super) async fn get(Path(post_id): Path<PostID>) -> Result<Response, StatusCode> {
     let file = match tokio::fs::read_to_string(
         std::path::Path::new(STORE_PATH)
             .join(&post_id)
@@ -21,5 +22,23 @@ pub(super) async fn get(Path(post_id): Path<PostID>) -> Result<String, StatusCod
         }
     };
 
-    Ok(file)
+    let html = match tokio::task::spawn_blocking(move || {
+        let arena = comrak::Arena::new();
+        let root = comrak::parse_document(&arena, &file, &comrak::Options::default());
+
+        let mut html = Vec::new();
+        comrak::format_html(root, &comrak::Options::default(), &mut html)?;
+        std::io::Result::Ok(html)
+    })
+    .await
+    .expect("task should not panic")
+    {
+        Ok(it) => it,
+        Err(err) => {
+            eprintln!("Couldn't post Markdown for post {post_id}: {err}");
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
+
+    Ok(([("Content-Type", "text/markdown")], Html(html)).into_response())
 }
