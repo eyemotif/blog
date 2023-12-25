32add669977f6896a93fd0713c095b3c392bb997
eyemotif <yepicness@gmail.com>
2023-12-25 13:32:15 -0500

    Refactor /thread

--- src/routes/api/post/thread.rs
+++ src/routes/api/post/thread.rs
@@ -3,54 +3,58 @@ use axum::extract::Path;
 use axum::http::StatusCode;
 use axum::Json;
 
-type LongestThreadFuture =
-    dyn std::future::Future<Output = Result<(usize, Post), StatusCode>> + Send;
+type LongestThreadFuture = dyn std::future::Future<Output = Result<Vec<Post>, StatusCode>> + Send;
 
 pub(super) async fn get(Path(post_id): Path<PostID>) -> Result<Json<Vec<Post>>, StatusCode> {
-    let (_, last_child) = longest_thread(post_id.clone()).await?;
+    let thread = longest_thread(post_id).await?;
 
-    let mut thread = vec![last_child];
-
-    loop {
-        let earliest_post = thread.last().unwrap();
-        if earliest_post.id == post_id {
-            break;
-        }
-
-        let Some(parent) = earliest_post.reply_to.as_ref() else {
-            break;
-        };
-        let parent = super::meta::get(Path(parent.clone())).await?.0;
-        thread.push(parent);
-    }
-
-    Ok(Json(thread))
+    // the previous implementation returned posts in reverse chonological order,
+    // and its easier to write a clean frontend that way as well
+    Ok(Json(thread.into_iter().rev().collect()))
 }
 
-// TODO: pass `Post` vectors up so `get` doesnt have to query them again
-fn longest_thread(post_id: PostID) -> std::pin::Pin<std::boxed::Box<LongestThreadFuture>> {
-    Box::pin(async move {
-        let post = super::meta::get(Path(post_id)).await?.0;
+async fn longest_thread(post_id: PostID) -> Result<Vec<Post>, StatusCode> {
+    fn longest_thread_inner(
+        parent_post: Post,
+    ) -> std::pin::Pin<std::boxed::Box<LongestThreadFuture>> {
+        Box::pin(async move {
+            let mut child_thread_set = tokio::task::JoinSet::new();
+            for child_id in parent_post.replies.clone() {
+                let child_post = super::meta::get(Path(child_id)).await?.0;
+                child_thread_set.spawn(longest_thread_inner(child_post));
+            }
 
-        let mut set = tokio::task::JoinSet::new();
-        for child_id in post.replies.clone() {
-            set.spawn(longest_thread(child_id));
-        }
+            let mut longest_child_thread: Option<Vec<Post>> = None;
+            while let Some(child_thread) = child_thread_set
+                .join_next()
+                .await
+                .transpose()
+                .expect("longest_thread_inner should not panic")
+                .transpose()?
+            {
+                longest_child_thread =
+                    Some(if let Some(longest_thread) = longest_child_thread.take() {
+                        if child_thread.len() > longest_thread.len() {
+                            child_thread
+                        } else {
+                            longest_thread
+                        }
+                    } else {
+                        child_thread
+                    });
+            }
 
-        let mut counts = Vec::new();
-        while let Some(count) = set
-            .join_next()
-            .await
-            .transpose()
-            .expect("count_children should not panic")
-            .transpose()?
-        {
-            counts.push(count);
-        }
+            let mut posts = vec![parent_post];
+            if let Some(mut longest_child_thread) = longest_child_thread {
+                posts.append(&mut longest_child_thread);
+            }
 
-        Ok(counts
-            .into_iter()
-            .max_by_key(|(depth, _)| *depth)
-            .unwrap_or((0, post)))
-    })
+            Ok(posts)
+        })
+    }
+
+    let post = super::meta::get(Path(post_id)).await?.0;
+    let posts = longest_thread_inner(post).await?;
+
+    Ok(posts)
 }
